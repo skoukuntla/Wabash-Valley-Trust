4ed1948cc366d1628a6e313bdc89fda8483cb168
JoeySpencer26 <spenc173@purdue.edu>
2022-11-12 13:03:26 -0500

    Made a sample modal to show functionality.

--- client/src/views/Main/Main.tsx
+++ client/src/views/Main/Main.tsx
@@ -1,7 +1,24 @@
 import { Box, Button, Container, Typography } from '@mui/material'
+import Modal from '@mui/material/Modal'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Main() {
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80%',
+    height: '80%',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  }
   return (
     <Container sx={{ py: 4 }}>
       <Typography variant="h4">Main Page</Typography>
@@ -20,16 +37,23 @@ export default function Main() {
           <li>
             <Link to="/auth">Auth</Link>
           </li>
-          <Button
-            variant="contained"
-            onClick={() => {
-              console.log('hello')
-              // show textbox
-            }}
-          >
-            Hello World
-          </Button>
         </ul>
+        <Button onClick={handleOpen}>Open modal</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
       </Box>
     </Container>
   )
