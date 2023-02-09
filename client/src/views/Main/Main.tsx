3ebda2aba24aa2b5ee42e015ec357e5a8330efd7
ishan <bhat52@purdue.edu>
2023-02-09 17:42:55 -0500

    Diamond achieved

--- client/src/views/Main/Main.tsx
+++ client/src/views/Main/Main.tsx
@@ -1,35 +1,56 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Container } from '@mui/material'
+import { Container, Grid, Slider } from '@mui/material'
 
 import mapImage from 'assets/tempmap.png'
 import { markers } from 'assets/tempmarkers'
 import Map from 'components/Map'
-import Slider from 'components/Slider'
-import React from 'react'
+
+const marks = [
+  { value: 0, label: '0' },
+  { value: 10, label: '10' },
+  { value: 20, label: '20' },
+  { value: 30, label: '30' },
+  { value: 40, label: '40' },
+  { value: 50, label: '50' },
+]
 
 export default function Main() {
   return (
     <Container>
-      {/* <Typography variant="h4">Main Page</Typography>
-      <Typography variant="body1">
-        Welcome! Feel free to check out all the demos here!
-      </Typography>
-      <Box sx={{ mt: 4 }}>
-        <Typography variant="h6">Demos</Typography>
-        <ul>
-          <li>
-            <Link to="/axios">Axios</Link>
-          </li>
-          <li>
-            <Link to="/redux">Redux</Link>
-          </li>
-          <li>
-            <Link to="/auth">Auth</Link>
-          </li>
-        </ul>
-      </Box> */}
+      <Grid container justifyContent="center">
+        <Slider
+          aria-label="Restricted values"
+          defaultValue={20}
+          step={null}
+          valueLabelDisplay="auto"
+          marks={marks}
+          max={50}
+          min={0}
+          sx={{
+            width: 300,
+            color: 'success.main',
+            '& .MuiSlider-thumb': {
+              width: 20,
+              height: 20,
+              backgroundColor: 'currentColor',
+              borderRadius: '0',
+              transform: 'rotate(45deg)',
+              shapeRendering: 'geometricPrecision',
+              borderTop: '10px solid transparent',
+              borderRight: '10px solid currentColor',
+              borderBottom: '10px solid transparent',
+              borderLeft: '10px solid transparent',
+            },
+            '& .MuiSlider-mark': {
+              borderRadius: '50%',
+              width: 8,
+              height: 8,
+            },
+          }}
+        />
 
-      <Map image={mapImage} markers={markers} />
+        <Map image={mapImage} markers={markers} />
+      </Grid>
     </Container>
   )
 }
