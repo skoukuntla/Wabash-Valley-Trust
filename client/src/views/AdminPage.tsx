cfdd17395618d82fd24c3da02beb9157b83392a5
Kai Tinkess <kaitinkess@gmail.com>
2023-04-17 20:12:11 -0400

    fixes

--- client/src/views/AdminPage.tsx
+++ client/src/views/AdminPage.tsx
@@ -240,7 +240,7 @@ const AdminPage = () => {
             </Button>
           </nav>
           <Map
-            image="/assets/map_clean.png"
+            image="assets/map_clean.png"
             markers={locations}
             addLocationLocally={addLocationLocally}
             deleteLocationLocally={deleteLocationLocally}
